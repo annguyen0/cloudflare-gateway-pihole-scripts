a0d757d9eabfed40832999a7cd9284fe2505d751
annguyen0 <phucan.nguyen@daum.net>
2023-09-17 10:23:37 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -4,6 +4,12 @@ source $(dirname "$0")/lib/helpers.sh
 
 # declare an array of urls
 urls=(
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_19_Annoyances_Popups/filter.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_21_Annoyances_Other/filter.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_22_Annoyances_Widgets/filter.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
