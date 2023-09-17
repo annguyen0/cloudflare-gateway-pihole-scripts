aad69397415bc7e93f0bc342f5b2fe8b72be21ac
annguyen0 <phucan.nguyen@daum.net>
2023-09-17 03:44:52 +0000

    delete redundent rules

---
+++ cf_gateway_rule_delete_added.js
@@ -0,0 +1,12 @@
+import { deleteZeroTrustRule, getZeroTrustRules } from './lib/api.js';
+
+;(async() => {
+    const { result: rules } = await getZeroTrustRules();
+    const [filtered_rule] = rules.filter(rule => rule.name === "oisd_hosts_full");
+
+    if (!filtered_rule) return console.warn("No rule with matching name found - this is not an issue if you haven't run the create script yet. Exiting.");
+
+    console.log(`Deleting rule`, process.env.CI ? "(redacted, running in CI)" : `"${filtered_rule.name}" with ID ${filtered_rule.id}`);
+
+    await deleteZeroTrustRule(filtered_rule.id);
+})();
---
+++ cf_list_delete_added.js
@@ -0,0 +1,18 @@
+import { deleteZeroTrustListsAtOnce, deleteZeroTrustListsOneByOne, getZeroTrustLists } from "./lib/api.js";
+import { FAST_MODE } from "./lib/constants.js";
+
+;(async() => {
+    const { result: lists } = await getZeroTrustLists();
+    if (!lists) return console.warn("No file lists found - this is not an issue if it's your first time running this script. Exiting.");
+    const cgps_lists = lists.filter(list => list.name.startsWith('oisd_hosts_full'));
+    if (!cgps_lists.length) return console.warn("No lists with matching name found - this is not an issue if you haven't created any filter lists before. Exiting.");
+
+    if (!process.env.CI) console.log(`Got ${lists.length} lists, ${cgps_lists.length} of which are CGPS lists that will be deleted.`);
+
+    if (FAST_MODE) {
+      await deleteZeroTrustListsAtOnce(cgps_lists);
+      return;
+    }
+
+    await deleteZeroTrustListsOneByOne(cgps_lists);
+})();
